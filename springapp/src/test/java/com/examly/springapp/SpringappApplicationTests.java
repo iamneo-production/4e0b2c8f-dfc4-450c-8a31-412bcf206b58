d7a3ad8229db45fbb76a271f4ad4333e4dc0aafa
UdhayakumarThangavel <dev.udhayakumar@gmail.com>
2023-05-06 11:24:37 +0000

    SpringApp initialized

---
+++ springapp/src/test/java/com/examly/springapp/SpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examly.springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
