f1a709dfdd6b3492203601caf55224502e1c58fc
UdhayakumarThangavel <dev.udhayakumar@gmail.com>
2023-06-27 05:55:27 +0000

    Renamed files  for testcases

--- springapp/src/main/java/com/example/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -1,4 +1,4 @@
-package com.examly.springapp;
+package com.example.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
