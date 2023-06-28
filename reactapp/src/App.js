d695620127fe4d482125c27d04de13b467d09f3d
roopikaar <roopikaaraghu1807@gmail.com>
2023-06-28 23:23:40 +0530

    Added Report UI Screen and Components

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,6 +1,7 @@
 import LandingScreen from './screens/LandingScreen';
 import DashboardScreen from './screens/DashboardScreen';
 import AccountScreen from "./screens/AccountScreen";
+import ReportScreen from './screens/ReportScreen';
 import {Route,Routes, Navigate, BrowserRouter} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import React from "react";
@@ -15,6 +16,7 @@ function App() {
               <Route path='/dashboard' element={<RequireAuth><DashboardScreen/></RequireAuth>}/>
               <Route path='/test' element={<DashboardScreen/>}/>
               <Route path='/account' element={<AccountScreen/>}/>
+              <Route path='/report' element={<ReportScreen/>}/>
           </Routes>
       </BrowserRouter>
 
