1ab19ac08624bd85561d3721686665342d6d39da
aniktarafder30 <anik.tarafder.30@gmail.com>
2023-07-04 03:14:24 +0530

    update

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -7,7 +7,7 @@ import {Route,Routes, Navigate, BrowserRouter} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import React from "react";
 import {validateToken} from "./features/userSlice";
-
+import BudgetScreen from "./screens/BudgetScreen";
 function App() {
 
   return (
@@ -15,10 +15,10 @@ function App() {
           <Routes>
               <Route exact path='/' element={<AlreadyLoggedin><LandingScreen></LandingScreen></AlreadyLoggedin>} />
               <Route path='/dashboard' element={<RequireAuth><DashboardScreen/></RequireAuth>}/>
-              <Route path='/test' element={<DashboardScreen/>}/>
               <Route path='/account' element={<AccountScreen/>}/>
               <Route path='/report' element={<ReportScreen/>}/>
               <Route path='/goal' element={<GoalScreen/>}/>
+              <Route path='/budget' element={<BudgetScreen/>}/>
           </Routes>
       </BrowserRouter>
 
