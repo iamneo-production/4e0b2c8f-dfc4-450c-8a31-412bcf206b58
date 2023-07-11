ee0db0762e58aed778bd0406f2b49e737edf159e
roopikaar <roopikaaraghu1807@gmail.com>
2023-07-11 11:41:15 +0530

    Updated report with api

--- reactapp/src/components/reports/ReportList.js
+++ reactapp/src/components/reports/ReportList.js
@@ -1,31 +1,46 @@
 import { Text,Table } from '@mantine/core';
 import { ReactComponent as DownloadSVG } from '../../assets/Import.svg';
-
+import {useSelector} from "react-redux";
+import axios from "axios";
+import {baseUrl} from "../../api/config";
+import { saveAs } from 'file-saver';
 
 export default function ReportList() {
 
-  const list = ["Transactions Report","Budgets Report","Goals Report","Debts Report"]
+    const list = ["Transactions Report","Budgets Report","Goals Report","Debts Report"]
+    const token = useSelector(state => state.user.token)
 
-  const rows = list.map((element) => (
-    
-     <tr key={element.name}>
-     <td><Text  fw={700} >{element}</Text></td>
-     <td>{<DownloadSVG/>}</td>
-   </tr>
-    
-  ));
+    async function handleTransactionReportExcel() {
+        try {
+            console.log("1");
+            const response = await axios.get(`${baseUrl}/report/transaction/excel`, {
+                headers: { Authorization: `Bearer ${token}` },
+                responseType: 'blob', // Set the response type to blob
+            });
+            const date = new Date()
+            saveAs(response.data, `TransactionReport_${date.toLocaleDateString()}.xlsx`);
+        } catch (error) {
+            console.error('Error downloading Excel file:', error);
+        }
+    }
 
-  return (
-    <div style={{margin:30}}>
-      <Table verticalSpacing="lg">
-        <thead>
-          <tr>
-            <th><Text c="dimmed">NAME</Text></th>
-            <th><Text c="dimmed">DOWNLOAD</Text></th>
-          </tr>
-        </thead>
-        <tbody >{rows}</tbody>
-      </Table>
-    </div>
-  )
+
+    return (
+        <div style={{margin:30}}>
+            <Table verticalSpacing="lg">
+                <thead>
+                <tr>
+                    <th><Text c="dimmed">NAME</Text></th>
+                    <th><Text c="dimmed">DOWNLOAD</Text></th>
+                </tr>
+                </thead>
+                <tbody >
+                <tr>
+                    <td><Text  fw={700} >Transaction Report</Text></td>
+                    <td>{<DownloadSVG onClick={() => handleTransactionReportExcel()}/>}</td>
+                </tr>
+                </tbody>
+            </Table>
+        </div>
+    )
 }
