35baf9f4eab413ecef66081380cae6bd20734c61
aniktarafder30 <anik.tarafder.30@gmail.com>
2023-07-12 16:29:56 +0530

    Budget ui and edit button updated but button needs fix

--- reactapp/src/components/budget/BudgetList.js
+++ reactapp/src/components/budget/BudgetList.js
@@ -1,34 +1,44 @@
-
-import {Table, Text} from "@mantine/core";
+import {useState} from "react";
+import {Avatar, Button, Group, Table, Text, UnstyledButton} from "@mantine/core";
 import { ReactComponent as EditSVG } from '../../assets/Edit.svg';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {showBudgetForm} from "../../features/budgetSlice";
+import BudgetForm from "./BudgetForm";
 
 export default function BudgetList(){
-    const budgetList = useSelector(state => state.budget.budgetList)
+        const dispatch= useDispatch()
+
+        const [rowToEdit,setRowToEdit]=useState(null)
+
+    //needs fix
+    const handleEditRow = (idx) =>{setRowToEdit(idx); dispatch(showBudgetForm())}
+
+        const budgetList = useSelector(state => state.budget.budgetList)
         const rows = budgetList.map((element) => (
         <tr key={element.budgetId}>
             <td><Text fw={700}> {element.category.name} </Text></td>
             <td><Text fw={700}> {element.amount} </Text></td>
-            <td><Text fw={700}> {element.used} (.....ui element goes here) </Text></td>
+            <td><Text fw={700}> {element.used} (.....progress bar) </Text></td>
             <td><Text fw={700}> {element.left} </Text></td>
-            <td>{<EditSVG/>}</td>
+            <td><Button variant="subtle" leftIcon={<EditSVG/>} onClick={()=>handleEditRow()}/></td>
         </tr>
     ));
 
     return (
         <div style={{margin:20}}>
-            <Table verticalSpacing="md">
+            <Table verticalSpacing="md" >
                 <thead>
                 <tr>
-                    <th> <Text fw={500} c="dimmed">NAME</Text> </th>
-                    <th> <Text fw={500} c="dimmed">BUDGET</Text> </th>
-                    <th><Text fw={500} c="dimmed">USED</Text></th>
-                    <th> <Text fw={500} c="dimmed">LEFT</Text></th>
+                    <th> <Text c="dimmed">NAME</Text> </th>
+                    <th> <Text c="dimmed">BUDGET</Text> </th>
+                    <th> <Text c="dimmed">USED</Text></th>
+                    <th> <Text c="dimmed">LEFT</Text></th>
                 </tr>
                 </thead>
                 <tbody>{rows}</tbody>
             </Table>
         </div>
     );
+
 
 }
