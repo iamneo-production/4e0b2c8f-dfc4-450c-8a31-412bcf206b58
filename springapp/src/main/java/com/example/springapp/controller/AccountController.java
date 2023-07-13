8eabfcd489b24099a9d011f3e4e8c0f862c5689f
UdhayakumarThangavel <dev.udhayakumar@gmail.com>
2023-07-13 11:15:39 +0530

    Updated Account

--- springapp/src/main/java/com/example/springapp/controller/AccountController.java
+++ springapp/src/main/java/com/example/springapp/controller/AccountController.java
@@ -27,6 +27,13 @@ public class AccountController {
         accountService.addAccount(account,userName);
         return new BaseResponceDto("success");
     }
+
+    @PutMapping("/api/accounts")
+    public BaseResponceDto updateAccount(@RequestHeader(value = "Authorization", defaultValue = "") String token,@RequestBody Account account,@RequestParam String accountId){
+        accountService.updateAccount(account,Integer.valueOf(accountId));
+        return new BaseResponceDto("success");
+    }
+
     @GetMapping("/api/accounts")
     public BaseResponceDto getAccount(@RequestHeader(value = "Authorization", defaultValue = "") String token){
         String userName = jwtGenerator.getUsernameFromJWT(jwtGenerator.getTokenFromHeader(token));
@@ -34,11 +41,11 @@ public class AccountController {
         return new BaseResponceDto("success",accounts);
     }
     @DeleteMapping("/api/accounts")
-    public BaseResponceDto deleteAccount(@RequestHeader(value = "Authorization", defaultValue = "") String token,@PathVariable String account_id){
+    public BaseResponceDto deleteAccount(@RequestHeader(value = "Authorization", defaultValue = "") String token,@RequestParam String accountId){
         String userName = jwtGenerator.getUsernameFromJWT(jwtGenerator.getTokenFromHeader(token));
-        if(accountService.hasAccount(account_id)){
-            if(accountService.hasPermission(userName,account_id)){
-                accountService.deleteAccount(account_id);
+        if(accountService.hasAccount(accountId)){
+            if(accountService.hasPermission(userName,accountId)){
+                accountService.deleteAccount(accountId);
                 return new BaseResponceDto("success");
             }else {
                 return new BaseResponceDto("couldn't delete account");
